cae55b2c52273d4a65839cde0aa400e78fb396b0
szankacsanad <cs.szanka@gmail.com>
2025-02-27 14:01:45 +0100

    sql hozzácsatolása a javához

--- java asztali/Teak/Teak-main/src/main/java/module-info.java
+++ java asztali/Teak/Teak-main/src/main/java/module-info.java
@@ -2,8 +2,11 @@ module com.example.Teak {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires java.sql;
+
 
 
     opens com.example.Teak to javafx.fxml;
     exports com.example.Teak;
 }
+
